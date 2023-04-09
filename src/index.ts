6cb43db2e700e03efd41c08a6a9027fd04cee538
noahgreff <greffnoah@gmail.com>
2023-04-09 23:04:05 +0200

    added `amqp`

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,33 @@
 import http from 'http'
 import * as WebSocket from 'ws'
+import amqp from 'amqplib'
 
-const server = http.createServer()
-const wss = new WebSocket.Server({ server })
+const clipboardQueue = 'clipboard-queue'
+const clipboardMessage = 'clipboard-message'
 
-wss.on('connection', (ws: WebSocket) => {
-	ws.on('message', (message: string) => {
-		console.log(JSON.parse(message))
+const main = async () => {
+	const connection = await amqp.connect('amqp://localhost')
+	const channel = await connection.createChannel()
 
-		wss.clients.forEach(client => {
-			if (client.readyState && client != ws) {
-				client.send(message)
-			}
+	await channel.assertQueue(clipboardQueue)
+
+	const server = http.createServer()
+	const wss = new WebSocket.Server({ server })
+
+	wss.on('connection', (ws: WebSocket) => {
+		ws.on('message', (message: string) => {
+			console.log(JSON.parse(message))
+
+			wss.clients.forEach(async client => {
+				if (client.readyState && client != ws) {
+					channel.publish(clipboardMessage, 'clipboard', Buffer.from(message))
+					client.send(message)
+				}
+			})
 		})
 	})
-})
 
-server.listen(8000, () => console.log('Started!'))
+	server.listen(8000)
+}
+
+main()
