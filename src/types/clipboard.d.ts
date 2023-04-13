230b4fe6ce3d9d2b2021a069598a65e9b68a4314
noahgreff <greffnoah@gmail.com>
2023-04-13 20:13:24 +0200

    updated types

--- src/types/clipboard.d.ts
+++ src/types/clipboard.d.ts
@@ -1,11 +1,8 @@
-export default interface Clipboard {
-	type: ClipboardType
-	content: string | File
-}
-
 export type ClipboardType = 'text' | 'file' | 'folder' | 'diverse'
 
-export type File = {
-	contentId: string
-	name: string
+export type Record = {
+	id: string
+	date: string
+	type: ClipboardType
+	content: string
 }
