58ad67f71d7a48535af3fee671f4a3a5f648708a
noahgreff <greffnoah@gmail.com>
2023-04-13 20:14:00 +0200

    updated rabbitmq

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,29 @@
 import http from 'http'
 import * as WebSocket from 'ws'
 import amqp from 'amqplib'
-import Clipboard from './types/clipboard'
-import { File } from './types/clipboard'
 
 const clipboardQueue = 'clipboard-queue'
-const clipboardExchange = 'clipboard-message'
 
 const main = async () => {
 	const connection = await amqp.connect('amqp://localhost')
 	const channel = await connection.createChannel()
 	await channel.assertQueue(clipboardQueue)
-	await channel.assertExchange(clipboardExchange, 'topic')
 
 	const server = http.createServer()
 	const wss = new WebSocket.Server({ server })
 
-	wss.on('connection', (ws: WebSocket) => {
-		ws.on('message', (message: string) => {
-			const clipboard = JSON.parse(message) as Clipboard
+	channel.consume(clipboardQueue, message => {
+		if (message.fields.routingKey === 'record.created') {
+			console.log(JSON.parse(message.content.toString()))
 
-			const { type, content } = clipboard
-			const dto = {
-				type,
-				content: type === 'text' ? content : (content as File).contentId
+			for (const client of wss.clients) {
+				client.send(message.content.toString())
 			}
-			const buffer = Buffer.from(JSON.stringify(dto))
-			channel.publish(clipboardExchange, 'clipboard', buffer)
-
-			wss.clients.forEach(async client => {
-				if (client.readyState && client != ws) {
-					client.send(message)
-				}
-			})
-		})
+			channel.ack(message)
+		}
 	})
 
-	server.listen(8000, '192.168.0.200', () => console.log('Started!'))
+	server.listen(8000, () => console.log('Started!'))
 }
 
 main()
