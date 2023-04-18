34fb1da9e88802026c828cd0452fb1dc40d4bdd9
noahgreff <greffnoah@gmail.com>
2023-04-18 20:33:14 +0200

    update logic to work with expressws

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-import http from 'http'
-import * as WebSocket from 'ws'
+import express from 'express'
+import addWsRoute from 'express-ws'
+import { ExpressWebSocket } from './global'
 import amqp from 'amqplib'
+import Record from './types/record'
+import Client from './types/client'
 
 const clipboardQueue = 'clipboard-queue'
+
+const app = express() as ExpressWebSocket
+const wss = addWsRoute(app).getWss()
+app.use(express.json())
+
+const clients: Client[] = []
 
 const main = async () => {
 	const { RABBITMQ_USERNAME, RABBITMQ_PASSWORD, RABBITMQ_HOSTNAME } = process.env
@@ -12,21 +21,32 @@ const main = async () => {
 	const channel = await connection.createChannel()
 	await channel.assertQueue(clipboardQueue)
 
-	const server = http.createServer()
-	const wss = new WebSocket.Server({ server })
-
 	channel.consume(clipboardQueue, message => {
 		if (message.fields.routingKey === 'record.created') {
-			console.log(JSON.parse(message.content.toString()))
+			const raw = message.content.toString()
+			const record = JSON.parse(raw) as Record
+			console.log(record)
 
-			for (const client of wss.clients) {
-				client.send(message.content.toString())
+			const sender = '3fa85f64-5717-4562-b3fc-2c963f66afa6' // TODO - record.userId
+
+			for (const client of clients) {
+				if (client.id === sender) {
+					client.socket.send(raw)
+				}
 			}
 			channel.ack(message)
 		}
 	})
 
-	server.listen(8000, () => console.log('Started!'))
+	app.ws('/:id', (ws, req) => {
+		console.log('New connection:', req.params.id)
+		clients.push({
+			id: req.params.id,
+			socket: ws
+		})
+	})
+
+	app.listen(8000, () => console.log('Started!'))
 }
 
 main()
